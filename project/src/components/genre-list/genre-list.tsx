ffbf6ac0bb3b882a8ae55b3998b50d0ce9c72197
Connant <diconna@gmail.com>
2021-11-02 19:55:42 +0700

    fixed genres

--- project/src/components/genre-list/genre-list.tsx
+++ project/src/components/genre-list/genre-list.tsx
@@ -1,9 +1,7 @@
 import { Film } from '../film-card/film-card';
 import { changeGenre } from '../../store/action';
 import { State } from '../reducer/reducer';
-import { Genres } from '../../const';
 import { useDispatch, useSelector } from 'react-redux';
-
 
 export type GenreListProps = {
   films: Film[],
@@ -17,10 +15,13 @@ export default function GenreList({ films }: ConnectedGenreListProps): JSX.Eleme
   const dispatch = useDispatch();
   const filmList = useSelector((state: State) => state.currentFilm);
   const currentGenre = useSelector((state: State) => state.currentGenre);
-  const genres = [Genres, ...new Set(filmList.map((it) => it.genre))] as string[];
+  const genres = [...new Set(filmList.map((it) => it.genre))] as string[];
 
   return (
     <ul className="catalog__genres-list">
+      <li className="catalog__genres-item catalog__genres-item--active">
+        <a href="/" className="catalog__genres-link">All genres</a>
+      </li>
       {genres.map((genre) => (
         <li className={`catalog__genres-item ${genre === currentGenre && 'catalog__genres-item--active'}`}
           key={genre}
